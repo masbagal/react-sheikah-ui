35c989d75b3d2238adf24f5d1b2f7571e1972e8e
masbagal <dimitrij.agal@gmail.com>
2021-02-11 19:44:31 +0700

    export

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export { default as Button } from './components/Button/Button';
 export { default as RoundedButton } from './components/RoundedButton/RoundedButton';
 export { default as Card } from './components/Card/Card';
 export { default as DialogueCard } from './components/DialogueCard/DialogueCard';
+export { default as Toast } from './components/Toast/Toast';
+export { StyleWrapper, useTheme } from './components/StyleWrapper';
